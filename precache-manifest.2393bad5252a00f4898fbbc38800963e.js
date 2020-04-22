6530a1411ee6e29890b93cfe994ada9701aec876
deathcoder <kartikeya72001@gmail.com>
2020-04-22 11:55:35 +0530

    Updates

---
+++ precache-manifest.2393bad5252a00f4898fbbc38800963e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df21ee2a6775aaedf9aa3cfcbb120d10",
+    "url": "/IncampusReact/index.html"
+  },
+  {
+    "revision": "53401b825c0521755a3f",
+    "url": "/IncampusReact/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0df7657831475c21744c",
+    "url": "/IncampusReact/static/css/main.fc19dd56.chunk.css"
+  },
+  {
+    "revision": "53401b825c0521755a3f",
+    "url": "/IncampusReact/static/js/2.9f64eeaf.chunk.js"
+  },
+  {
+    "revision": "e200835c9f9c7469794ddfa05d78c2ac",
+    "url": "/IncampusReact/static/js/2.9f64eeaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0df7657831475c21744c",
+    "url": "/IncampusReact/static/js/main.8d078fca.chunk.js"
+  },
+  {
+    "revision": "2e4ee73277987a0ae555",
+    "url": "/IncampusReact/static/js/runtime-main.a4cacbf5.js"
+  },
+  {
+    "revision": "9debf296ed787cd0bb7dda67fc0c62db",
+    "url": "/IncampusReact/static/media/Logo.9debf296.PNG"
+  },
+  {
+    "revision": "a3f9c50340b8e2b3530687dd67d085f3",
+    "url": "/IncampusReact/static/media/OTP anime.a3f9c503.gif"
+  },
+  {
+    "revision": "e6d61c9749082d9c939c4cb66340110e",
+    "url": "/IncampusReact/static/media/SecProfilePic.e6d61c97.jpg"
+  }
+]);
